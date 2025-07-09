5dedbcc390c08ac6cecbacc34e843323f4ccb10f
bhargu <kbv202002@gmail.com>
2025-07-09 09:10:32 +0530

    revert

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 app = Flask(__name__)
 app.secret_key = 'your_secret_key_here' # Change this to a strong, random key!
 
-# --- Routes --- 
-
+# --- Routes ---
 
 @app.route('/')
 def home():
